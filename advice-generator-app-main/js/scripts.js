2e98f5e39ec4830ed603ab10a609d2c5f3850ed5
HilarioGo <hilario.gonzalez.jr@gmail.com>
2022-03-14 00:00:16 -0400

    Advice Generate App Initial Commit

---
+++ advice-generator-app-main/js/scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.adviceslip.com/advice');
+    request.send();
+    request.onload = updateAdvice;
+
+    document.getElementById('button').addEventListener('click', updateAdvice);
+});
+
+function updateAdvice() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.adviceslip.com/advice');
+    request.send();
+    request.onload = function() {
+        console.log(request);
+        if (request.status === 200) {
+            console.log(JSON.parse(request.response));
+
+            document.getElementById('AdviceID').innerHTML = JSON.parse(request.response).slip.id;
+            document.getElementById('Advice').innerHTML = JSON.parse(request.response).slip.advice;
+        } else {
+            console.log('error ${request.status} ${request.statusText}');
+        }
+    }
+}

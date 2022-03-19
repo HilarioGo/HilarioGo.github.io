6b74cf12cafb02fc328ba1208e6ff90f1501caa2
HilarioGo <hilario.gonzalez.jr@gmail.com>
2022-03-18 22:33:44 -0400

    Fixes
    Fixed font link, added main element, removed debug logging and adjusted the size of the Desktop layout card.

--- advice-generator-app-main/js/scripts.js
+++ advice-generator-app-main/js/scripts.js
@@ -9,10 +9,7 @@ function updateAdvice() {
     request.open('GET', 'https://api.adviceslip.com/advice?t=' + Math.random(), true);
     request.send();
     request.onload = function() {
-        console.log(request);
         if (request.status === 200) {
-            console.log(JSON.parse(request.response));
-
             document.getElementById('AdviceID').innerHTML = JSON.parse(request.response).slip.id;
             document.getElementById('Advice').innerHTML = JSON.parse(request.response).slip.advice;
         } else {
